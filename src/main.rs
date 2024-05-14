8630f0a788f767ef1ae3f487e5ef46d8e230518d
GyroZepelix <gamingskullgamer@gmail.com>
2024-05-14 20:02:35 +0200

    Learning Leptos: reactive components

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use leptos::{component, create_signal, mount_to_body, view, IntoView, SignalGet, SignalUpdate};
+
 fn main() {
-    println!("Hello, world!");
+    console_error_panic_hook::set_once();
+    mount_to_body(|| {
+        view! {
+            <App/>
+        }
+    });
+}
+
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = create_signal(0);
+
+    view! {
+        <button
+            on:click=move |_| {
+                set_count.update(|n| *n += 1);
+            }
+            class:red=move || count.get() % 2 == 1
+        >
+            "Click me: "
+            {count}
+        </button>
+    }
 }
