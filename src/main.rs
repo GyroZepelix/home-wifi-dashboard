f87dd4f8ed5b9db941b2ce5488dd24bd74dadbe1
GyroZepelix <gamingskullgamer@gmail.com>
2024-05-15 17:18:00 +0200

    Learning Leptos: Dynamic Iterator Rendering

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use leptos::{
-    component, create_signal, mount_to_body, view, CollectView, IntoView, Signal, SignalGet, SignalUpdate,
+    component, create_signal, mount_to_body, view, CollectView, For, IntoView, Signal, SignalGet, SignalUpdate, WriteSignal
 };
 
 fn main() {
@@ -27,6 +27,8 @@ fn App() -> impl IntoView {
         <ProgressBar progress=count/>
         <ProgressBar progress=Signal::derive(double_count)/>
         <StaticIteratorComponent/>
+        <br/>
+        <DynamicIteratorComponent initial_lenght=3/>
     }
 }
 
@@ -48,6 +50,23 @@ fn ProgressBar(
     }
 }
 
+#[component]
+fn Counter(
+    #[prop(into)]
+    count: Signal<u16>,
+    set_count: WriteSignal<u16>,
+) -> impl IntoView {
+    view! {
+        <button
+            on:click=move |_| {
+                set_count.update(|n| *n += 1);
+            }
+        >
+            {count}
+        </button>
+    }
+}
+
 /// A list of counters, without the ability
 /// to add or remove any.
 #[component]
@@ -61,11 +80,7 @@ fn StaticIteratorComponent(
         .map(|(count, set_count)| {
             view! {
                 <li>
-                    <button
-                        on:click=move |_| set_count.update(|n| *n += 1)
-                    >
-                        {count}
-                    </button>
+                    <Counter count=count set_count=set_count/>
                 </li>
             }
         })
@@ -76,4 +91,58 @@ fn StaticIteratorComponent(
     }
 }
 
+/// A list of counters that allows you to add or remove counters.
+#[component]
+fn DynamicIteratorComponent(
+    /// The number of counters to begin with
+    initial_lenght: u16,
+) -> impl IntoView {
+    let mut next_counter_id = initial_lenght;
 
+    let initial_counters = (0..initial_lenght)
+        .map(|id| (id, create_signal(id + 1)))
+        .collect::<Vec<_>>();
+
+    let (counters, set_counters) = create_signal(initial_counters);
+
+    let add_counter = move |_| {
+        let sig = create_signal(next_counter_id + 1);
+        set_counters.update(move |counters| {
+            counters.push((next_counter_id, sig))
+        });
+
+        next_counter_id += 1;
+    };
+
+    let remove_counter = move |id| {
+        set_counters.update(|cntr| {
+            cntr.retain(|(counter_id, _)| *counter_id != id)
+        })
+    };
+
+    view! {
+        <div>
+            <button on:click=add_counter>
+                "Add Counter"
+            </button>
+            <ul>
+                <For
+                    each=move || counters.get()
+                    key=|counter| counter.0
+                    children=move |(id, (count, set_count))| {
+                        view! {
+                            <li>
+                                <Counter count=count set_count=set_count/>
+                                <button on:click=move |_| {
+                                    remove_counter(id)
+                                }>
+                                    "Remove"
+                                </button>
+                            </li>
+                        }
+                    }
+                />
+            </ul>
+        </div>
+    }
+}
