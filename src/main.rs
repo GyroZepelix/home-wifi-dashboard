1a66586768f77bbfadbd08c39e0dbaead53cacbc
GyroZepelix <gamingskullgamer@gmail.com>
2024-05-15 16:15:32 +0200

    Learning Leptos: Static Iterator Rendering

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use leptos::{component, create_signal, mount_to_body, view, IntoView, ReadSignal, Signal, SignalGet, SignalUpdate};
+use leptos::{
+    component, create_signal, mount_to_body, svg::view, view, CollectView, IntoView, ReadSignal,
+    Signal, SignalGet, SignalUpdate,
+};
 
 fn main() {
     console_error_panic_hook::set_once();
@@ -24,6 +27,7 @@ fn App() -> impl IntoView {
         </button>
         <ProgressBar progress=count/>
         <ProgressBar progress=Signal::derive(double_count)/>
+        <StaticIteratorComponent/>
     }
 }
 
@@ -31,14 +35,39 @@ fn App() -> impl IntoView {
 #[component]
 fn ProgressBar(
     /// The maximum value of the progress bar.
-    #[prop(default = 100)] max: u16,
+    #[prop(default = 100)]
+    max: u16,
     /// The current progress value.
-    #[prop(into)] progress: Signal<i32>,
+    #[prop(into)]
+    progress: Signal<i32>,
 ) -> impl IntoView {
     view! {
         <progress
             max=max
             value=progress
         />
+    }
+}
+
+#[component]
+fn StaticIteratorComponent(#[prop(default = 5)] lenght: u16) -> impl IntoView {
+    let counters = (1..=lenght).map(|idx| create_signal(idx));
+
+    let counter_buttons = counters
+        .map(|(count, set_count)| {
+            view! {
+                <li>
+                    <button
+                        on:click=move |_| set_count.update(|n| *n += 1)
+                    >
+                        {count}
+                    </button>
+                </li>
+            }
+        })
+        .collect_view();
+
+    view! {
+        <ul>{counter_buttons}</ul>
     }
 }
